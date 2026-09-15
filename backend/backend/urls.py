f282f781451e96324d54df56b54129e6704dd786
Abdulmumin Abubakar <114495965+codesmith-abba@users.noreply.github.com>
2026-09-15 06:55:07 +0100

    feat(backend): expose health check under API

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health/', include('network.api.urls')),
     path('api/', include('network.urls')),
 ]
