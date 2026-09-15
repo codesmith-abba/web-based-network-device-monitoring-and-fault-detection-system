e1a82d24848ddff50d9c1449c0ed7122899099f8
Abdulmumin Abubakar <114495965+codesmith-abba@users.noreply.github.com>
2026-09-15 20:29:49 +0100

    refactor: remove obsolete duplicate history and monitoring API views

--- backend/network/views.py
+++ backend/network/views.py
@@ -1,4 +1,3 @@
-from django.db import transaction
 from rest_framework import mixins, status, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
